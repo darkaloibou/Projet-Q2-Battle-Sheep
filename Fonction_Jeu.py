bfb718eea7cb025e9201eff6654aefd4c6000c2d
scottyhey <161167522+scottyhey@users.noreply.github.com>
2024-02-26 20:46:31 +0100

    Update Fonction_Jeu.py

--- Fonction_Jeu.py
+++ Fonction_Jeu.py
@@ -36,20 +36,15 @@ def is_game_over():
     -------
     specification : Heynen Scott-Socrate (v1 20/02/24)"""
 
-#
-def display_map(map,player,seed):
+
+def display_map():
     """display the map from the dictionary of create_map_dictio()
-    parameters
-    ----------
-    map : dictionary of the map with the size of the map, rocks, player spawn and seeds (dic)
-    players : dictionary of the different sheeps and their ownership (dic)
-    grass : dictionary of the grass and their ownership (dic)
 
     version
     -------
     specification: Aloïs Baurant (v1 23/02/24)
     """  
-#
+
 def try_spawn_sheep(sheep):
     """Spawn a sheep if possible
     parameters
@@ -67,7 +62,7 @@ def try_spawn_sheep(sheep):
     """
 
 def set_grass (coordinates):
-    """Set a grass at the sheep position if the seed is neutral
+    """Set a grass at the sheep position if the seed isn't already take by a player
     parameters
     -------------
     coordinates: coordinates of the sheep
@@ -82,7 +77,7 @@ def growth_grass():
     specification: Remacle Thomas (v1 24/02/24)"""
     
 def grass_propagation (coordinates):
-    """Plant grass ont the all 8 box surroundings
+    """Plant grass on the all 8 box surroundings
     parameters
     ----------
     coordinates : coordinates (x,y) of the grass that will grow (tuples)
@@ -90,21 +85,21 @@ def grass_propagation (coordinates):
     -------
     specification: Heynen Scott-Socrate (v1 23/02/24)
     """
-def delete_emoji (emoji_coordonate):
+def delete_emoji (emoji_coordinates):
     """delete the emoji we need 
     parameters
     ----------
-    emoji_coordonate: coordonate in x,y of the emoji we want to delete, in fact remplace by a double space (tuples or list depend if the emoji is element of 
-    the map or a sheep)
+    emoji_coordinates: coordinates (x,y) of the emoji to delete, in fact remplace by a double space (tuples or list depend if the emoji is element of 
+    the map or a sheep/grass)
     version
     -------
     specification: Heynen Scott-Socrate (v1 23/02/24)
     """
-def create_emoji (emoji_coordinate,emoji):
+def create_emoji (emoji_coordinates,emoji):
     """spawn the emoji we need 
     parameters
     ----------
-    emoji_coordinate: coordinate in x,y of the emoji wanted to be create (tuples or list depend if the emoji is element of the map or a sheep)
+    emoji_coordinates: coordinate in (x,y) of the emoji wanted to be created (tuples or list depend if the emoji is element of the map or a sheep/grass)
     emoji: the emoji wanted to be spawn (str)
     version
     -------
@@ -135,14 +130,14 @@ def move_sheep (old_coordinates,new_coordinates):
     """
 
 def sheep_graze(sheep, sheep_coordinates):
-     """Graze a grass if the sheep is on this case
+     """Graze a grass if the sheep is on this box
     parameters
     ----------
     sheep : player sheep (0 for player 1, 1 for player 2) (int)
-    sheep_coordinates: coordinates in x,y of the sheep (list)
+    sheep_coordinates: coordinates (x,y) of the sheep (list)
     return
     ------
-    emojii_deleted: coordinate in x and y of the grass that will be eaten (tuples)
+    emojii_deleted: coordinates (x,y) of the grass that will be eaten (tuples)
 
     version
     -------
@@ -153,7 +148,7 @@ def translate_orders (player,messsage):
     """Translate a string message into a list to be usable for the program
     parameters
     ----------
-    player : The player where the message from (bool)
+    player : The player that wrote the message, 0 if player 1 , 1 if player 2 (bool)
     message : get the player message (str)
     
     return 
@@ -171,56 +166,64 @@ def game_function ():
     ---------------
     specification: Remacle Thomas (V1 26/02/24)"""
 
-def look_rock(case_coordonate):
+def look_rock(case_coordinates):
     """Look if there are a rock on the case or not
+    
     parameters
     -----------
-    case_coordonate: The coordonate x and y of the case we wanted to see (tuples)
+    case_coordinates: The coordinates (x,y) of the case we wanted to check (tuples)
+    
     return
     -----------
-    type: return if there are a rock on the case or not 1 if True 0 if False (bool)
+    type: return 1 if there is a rock return 0 otherwise (bool)
+    
     version
     -------
     specification: Arthur Yernaux (v1 23/02/24)
     """
-def look_sheep(case_coordonate):
-    """Look if there are a sheep on the case or not.
+def look_sheep(box_coordinates):
+    """Look if there are a sheep on the case or not
     parameters
     -----------
-    case_coordonate: The coordonate x and y of the case we wanted to see (tuples)
+    box_coordinates: The coordinates (x,y) of the box to check (tuples)
+    
     return
     -----------
-    type: return if there are a sheep on the case or not 1 if True 0 if False (bool)
+    type: type: return 1 if there is a sheep return 0 otherwise (bool)
+    
     version
     -------
     specification: Arthur Yernaux (v1 23/02/24)
     """
 
-def look_seed(case_coordinates):
-    """Look if there are a seed on the case or not.
+def look_seed(box_coordinates):
+    """Look if there is a seed on the box or not.
     parameters
     -----------
-    case_coordinates: The coordinates (x,y) of the box wanted to be seen (tuples)
+    box_coordinates: The coordinates (x,y) of the box to check (tuples)
+    
     return
     -----------
     type: return if there are a seed on the case 1 if True 0 if False (bool)
+    
     version
     -------
     specification: Arthur Yernaux (v1 23/02/24)
     """
 
 def look_grass(coordinates):
-    """Look if there are a grass or not in this case.
+    """Look if there is a grass or not in this box.
     parameters
     -----------
-    coordinate: The coordinates (x,y) of the box we wanted to grow the grass (tuples)
+    coordinate: The coordinates (x,y) of the grass to grow (tuples)
     return
     -----------
-    type: return if there are a grass on the case or not 1 if True 0 if False (bool)
+    type: return 1 if there is a grass return 0 otherwise (bool)
     version
     -------
     specification: Arthur Yernaux (v1 23/02/24)
     """
+    
 def check_void(coordinates):
     """Look if the box is outside of the map
     -----------
