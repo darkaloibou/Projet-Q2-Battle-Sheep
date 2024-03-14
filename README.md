8d5e559177fdbaf4286bf6e501aed45c3ccb1c16
dark_aloibou <99367039+darkaloibou@users.noreply.github.com>
2024-03-14 09:57:47 +0100

    Supprime les fichiers inutile

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projet-Q2---Battel-Sheep
-Projet dans le cadre du cours du 2ᵉ quadri du bac 1 science informatiques. 
--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-# Définir le tuple initial
-mon_tuple = (16, 32)
-
-# Déballer les valeurs du tuple initial
-x, y = mon_tuple
-
-# Créer un nouveau tuple avec les valeurs modifiées
-nouveau_tuple = (x + 1, y + 5)
-
-# Afficher le nouveau tuple
-print(nouveau_tuple)
+# Faites vos testes python ici ;)
