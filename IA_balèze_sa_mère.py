d68576e8b33badd608a64e5210f5554208c52aef
scottyhey <161167522+scottyhey@users.noreply.github.com>
2024-03-28 12:31:40 +0100

    Update IA_balèze_sa_mère.py

--- IA_balèze_sa_mère.py
+++ IA_balèze_sa_mère.py
@@ -1,1 +1,41 @@
-#on va gagner !!!!
+def look_for_grass(sheep):
+    """return the best seed to go (2 seeds strategie)
+    
+    parameters
+    ----------
+    sheep: the sheep to move
+    
+    return
+    ------
+    the coordinates [x,y] of the seed to go (tuples)
+
+    version
+    -------
+    specification : Heynen Scott-Socrate (v2 28/03/24)  
+    """
+    
+    
+def find_path():
+    """"""
+    
+def attack(sheep1,sheep2):
+    """return the attack command 
+    
+    parameters
+    ----------
+    sheep1 : the (x,y) coordinates of the attacker sheep (tuples)
+    sheep2 : the (x,y) coordinates of the sheep that is attacked (tuples)
+    
+    returns
+    -------
+    orders: the order to add to the order list (str)
+    
+    version
+    -------
+    specification : Heynen Scott-Socrate (v2 28/03/24)"""
+
+def graze():
+    """"""
+    
+def move_sheep():
+    """"""
