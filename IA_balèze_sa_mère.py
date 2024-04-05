f47a89f8df551e7201641d185b0ee000dfb1dbd4
Azerty131131 <159807970+Azerty131131@users.noreply.github.com>
2024-04-05 19:11:45 +0200

    Update IA_balèze_sa_mère.py

--- IA_balèze_sa_mère.py
+++ IA_balèze_sa_mère.py
@@ -13,10 +13,10 @@ def get_distance(entity1_coordinates,entity2_coordinates):
     specification: Remacle Thomas (v1 24/02/24)
     implémentation : Remacle Thomas (v1 24/02/24)"""
     entity1_coordinates=[entity1_coordinates[0],entity1_coordinates[1]]
-    entity2_coordinates=[entity2_coordinates[0],entity2_coordinates[1]]
-    x_value=abs(entity2_coordinates[0]-entity1_coordinates[0])
-    y_value=abs(entity2_coordinates[1]-entity1_coordinates[1])
-    return_value=x_value+y_value
+    entity2_coordinates=[entity2_coordinates[0],entity2_coordinates[1]]  #Transform the coordinates
+    x_value=abs(entity2_coordinates[0]-entity1_coordinates[0]) #Calculate the distance between the coordinates_x
+    y_value=abs(entity2_coordinates[1]-entity1_coordinates[1]) #Calculate the distance between the coordinates_y
+    return_value=x_value+y_value  #Total distance 
     return return_value
 
 def look_for_grass(sheep):
@@ -35,12 +35,12 @@ def look_for_grass(sheep):
     specification : Heynen Scott-Socrate (v2 28/03/24)
     implémentation : Remacle Thomas (v1 24/02/24)
     """
-    seeds_dic =map['seed'].copy()
+    seeds_dic =map['seed'].copy()  #Copy the dictionnary of the seeds
     x_map=str(map['map_size'][0])
     x_map=int(x_map[0])
     y_map=str(map['map_size'][1])
     y_map=int(y_map[0])
-    minus=(x_map+y_map)/2
+    minus=(x_map+y_map)/2   #Calculate the minus, the minus is the tolarance of distance when you trying to find the seeds 
     minus=math.floor(minus)
     return_coordinate=(0,0)
     min_distance=100000
@@ -55,20 +55,19 @@ def look_for_grass(sheep):
             seeds_list=[]
             other_seed_list=[]
             seeds_list.append(seeds_dic[seeds])
-            other_seed_list.append(seeds)
+            other_seed_list.append(seeds)        # This loop finds the seed closest to the sheep
     min_distance=get_distance(sheep,return_coordinate)
-    del seeds_dic[other_seed_list[0]]
+    del seeds_dic[other_seed_list[0]]  #Delete the found seed in the seeds_dic dictionnary to avoid finding it again
     for seeds in seeds_dic:
         distance=get_distance(sheep,seeds_dic[seeds])
         if min_distance>=distance-minus:
             seeds_list.append(seeds_dic[seeds])
-            other_seed_list.append(seeds)
+            other_seed_list.append(seeds)   # This loop finds the seeds closer enough, the notion of close enough is derterminate with the minus variable
     other_seed_list=other_seed_list[1:]
     for seed in other_seed_list:
-        del seeds_dic[seed]
-    if len(seeds_list)<=2:
-        print("help")
-        return return_coordinate
+        del seeds_dic[seed]    #Delete the found seed in the seeds_dic dictionnary to avoid finding it again
+    if len(seeds_list)<2:
+        return return_coordinate   #If there are only one seed
     else:
         min_distance=[]
         for seeds in seeds_list:
@@ -77,21 +76,21 @@ def look_for_grass(sheep):
                 distance=get_distance(seeds,map['seed'][compare_seeds])
                 if not distance==0:
                     seed_min_distance.append(distance)
-            min_distance.append(min(seed_min_distance))
+            min_distance.append(min(seed_min_distance))    #Find the nearest seed from all the seed previously chose
         seed_min=min(min_distance)
         grass_coordinate=[]
-        count=0
+        count=0                                  
         for number in min_distance:
             if number==seed_min:
-                grass_coordinate.append(seeds_list[count])
+                grass_coordinate.append(seeds_list[count]) #Take the nearests seed
             count+=1
         min_distance=1000000
         return_coordinate=(0,0)
         for final_seed in grass_coordinate:
-            distance=get_distance(sheep,final_seed)
+            distance=get_distance(sheep,final_seed)  
             if min_distance>distance:
                 min_distance=distance
-                return_coordinate=final_seed
+                return_coordinate=final_seed   # Decide the seed of the sheep will
         return return_coordinate
     
     
