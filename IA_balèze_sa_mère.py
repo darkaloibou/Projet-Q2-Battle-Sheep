724e1e730b7e461dd2b7779903c99c51f9513a63
Azerty131131 <159807970+Azerty131131@users.noreply.github.com>
2024-04-20 19:08:45 +0200

    Update IA_balèze_sa_mère.py

--- IA_balèze_sa_mère.py
+++ IA_balèze_sa_mère.py
@@ -391,88 +391,13 @@ def search_attack(sheep):
     if old_distance > 2:
         return #false
     else:
-        move = find_path(sheep, old_pos)
+        move = move_ia(sheep, old_pos)
         return move_sheep(sheep, move)
         
     #orders += ' '+str(sheep[0])+'-'+str(sheep[1])+':*'
     #return orders
 
 
-def graze(sheep):
-    """return the graze command 
-    
-    parameters
-    ----------
-    sheep : the (x,y) coordinates of the sheep who graze (tuples)
-
-    returns
-    -------
-    orders: the order to add to the order list (str)
-
-    notes
-    -----
-    this function don't check if the order is valide
-    
-    version
-    -------
-    specification : Heynen Scott-Socrate (v2 28/03/24)
-    implementation : Heynen Scott-Socrate (v1 29/03/24)"""
-    grass_list=[]
-    sheep=[sheep[0],sheep[1]] # sheep
-    x_sheep = sheep[0] # coordinate of the sheep in x
-    x_sheep = sheep[1] # coordinate of the sheep in y
-    old_total = 0
-    total = 0
-    new = 0
-
-    # check with sheep is playing
-    for sheep_1 in players['player_1']['sheeps'] : # Player 1
-        sheep_1=[sheep[0],sheep[1]]
-        if sheep_1  == sheep:
-           sheep_player = 1
-
-    for sheep_2 in players['player_2']['sheeps'] : # Player 2
-        sheep_2=[sheep[0],sheep[1]]
-        if sheep_2 == sheep:
-           sheep_player = 2
-
-    # Create a list with all grass
-    if sheep_player == 1:
-        for i_grass in grass["player_1"]:
-            grass_list.append(i_grass)
-    else:
-        for i_grass in grass["player_2"]:
-            grass_list.append(i_grass)
-    # Check the grass witch one is the better
-    for i_grass in grass_list:
-        if new == 0: # Check if the function work for the first time
-            old_distance = i_grass
-            new = 1
-            for u_grass in grass_list: # Compute how mutch grass there is around this grass. 
-                    compute_grass = get_distance(i_grass,u_grass)
-                    if compute_grass <= 2: # If it is around the grass
-                        old_total += 1
-        else:
-            compute_new = get_distance(sheep,i_grass)
-            compute_old = get_distance(sheep,old_distance)
-            if compute_new==0 or compute_old == 0: # if sheep is on the grass
-                graze == 1
-            elif compute_new < compute_old: # Check if the new valor is better than the old one
-                old_distance = i_grass
-            elif compute_new == compute_old: # If the valor is the same
-                for u_grass in grass_list: # Compute how mutch grass there is around this grass. 
-                    compute_grass = get_distance(i_grass,u_grass)
-                    if compute_grass <= 2: # If it is around the grass
-                        total += 1
-                    if total > old_total: # if the total of grass is better than the old one ?
-                        old_total = total
-                        old_distance = i_grass
-    if graze == 1:
-        orders = ' '+str(sheep[0])+'-'+str(sheep[1])+':*'
-    else:
-        aim = move_ia(sheep,old_distance)
-        orders = ' '+str(old_coordinates[0])+'-'+str(old_coordinates[1])+':@'+str(aim[0])+'-'+str(aim[1])
-    return orders
 def move_sheep(old_coordinates, new_coordinates):
     """return the move command 
     
